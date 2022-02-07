fed00dfb56e5357e3a932a051bc03f15b483ffea
jjking7 <jj_king9@hotmail.co.uk>
2022-02-07 15:06:10 +0000

    read CRUD

--- src/main/java/com/qa/customers/service/CRUDServiceInterface.java
+++ src/main/java/com/qa/customers/service/CRUDServiceInterface.java
@@ -1,10 +1,15 @@
 package com.qa.customers.service;
 
+import java.util.List;
+
 public interface CRUDServiceInterface<T> {
 	
 	//CRUD
 	
 		//Create
 		T create(T t);
+		
+		//Read
+		List<T> readAll();
 
 }
