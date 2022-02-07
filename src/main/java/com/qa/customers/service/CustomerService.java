a5677665c72465a2c72da9b6e0b8a77df49d28d5
jjking7 <jj_king9@hotmail.co.uk>
2022-02-07 15:10:36 +0000

    created customerService

---
+++ src/main/java/com/qa/customers/service/CustomerService.java
@@ -0,0 +1,49 @@
+package com.qa.customers.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.customers.domain.Customer;
+import com.qa.customers.repo.CustomerRepo;
+
+@Service
+public class CustomerService implements CRUDServiceInterface<Customer> {
+
+	private CustomerRepo repo;
+	
+	public CustomerService(CustomerRepo repo) {
+		this.repo = repo;
+	}
+
+	@Override
+	public Customer create(Customer t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Customer> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer readById(int Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer update(int Id, Customer t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean delete(int Id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
