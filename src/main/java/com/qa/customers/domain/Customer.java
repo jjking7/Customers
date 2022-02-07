d9038fbd20c11c46cf164b27966181c1a0380cc6
jjking7 <jj_king9@hotmail.co.uk>
2022-02-07 14:48:55 +0000

    added getters and setters to domain

--- src/main/java/com/qa/customers/domain/Customer.java
+++ src/main/java/com/qa/customers/domain/Customer.java
@@ -32,4 +32,38 @@ public class Customer {
 	@Column
 	private String email;
 	
+	//Getters & Setters
+
+		public int getId() {
+			return Id;
+		}
+
+		public void setId(int Id) {
+			this.Id = Id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+		public String getEmail() {
+			return email;
+		}
+	
 }
