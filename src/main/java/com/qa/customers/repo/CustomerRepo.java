79117eefd26e658464a52c068c38701ee0413325
jjking7 <jj_king9@hotmail.co.uk>
2022-02-07 15:01:42 +0000

    extended jpa repo

--- src/main/java/com/qa/customers/repo/CustomerRepo.java
+++ src/main/java/com/qa/customers/repo/CustomerRepo.java
@@ -1,5 +1,9 @@
 package com.qa.customers.repo;
 
-public interface CustomerRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.qa.customers.domain.Customer;
+
+public interface CustomerRepo extends JpaRepository<Customer, Integer>{
 
 }
